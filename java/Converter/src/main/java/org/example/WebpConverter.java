8f74bc5c7d03727ff5eafb6ee8573a93bab3de6c
marhenkejanik <148453185+marhenkejanik@users.noreply.github.com>
2024-03-19 18:56:47 +0100

    Removed unused function and sleep

--- java/Converter/src/main/java/org/example/WebpConverter.java
+++ java/Converter/src/main/java/org/example/WebpConverter.java
@@ -23,7 +23,6 @@ public class WebpConverter
 
         while(true) {
             monitorFolder(sourceFolderPath, targetFolderPath);
-            sleepForSeconds(2); // Adding a 2-second delay so it doesnt convert a file that is being downloaded resulting in an error
         }
     }
 
@@ -53,7 +52,8 @@ public class WebpConverter
         }
     }
 
-    private static void convertWebpToPng(File webpFile, String targetFolderPath) {
+    private static void convertWebpToPng(File webpFile, String targetFolderPath)
+    {
         String inputFilePath = webpFile.getAbsolutePath();
         String outputFileName = webpFile.getName().replace(".webp", ".png");
         String outputFilePath = targetFolderPath + File.separator + outputFileName;
@@ -64,16 +64,5 @@ public class WebpConverter
         // Convert the WebP Mat to PNG format
         Imgcodecs.imwrite(outputFilePath, webpMat);
         webpFile.delete();
-    }
-    private static void sleepForSeconds(int seconds)
-    {
-        try
-        {
-            TimeUnit.SECONDS.sleep(seconds);
-        }
-        catch (InterruptedException e)
-        {
-            Thread.currentThread().interrupt();
-        }
     }
 }
